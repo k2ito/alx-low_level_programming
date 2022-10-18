72f0c21d953d9c1b33a3388a106cb17ac2aff335
Abdulrahman <abduhassan6565@gmail.com>
2022-10-18 13:58:49 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints _putchar, fllowd by a new linees 
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	char arr[] = "_putchar";
+
+	int i;
+
+	for (i=0; i < 8; i++)
+	{
+		_putchar(arr[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: mian.h
+ *
+ * Desc: Header file containing declartion for all functions
+ */
+
+int _putchar(char c);
+#endif
